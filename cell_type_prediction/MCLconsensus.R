51fa48c634592c4dde7fde6c86cc64339b8d81d7
qibaotu <juanxie.84@gmail.com>
2018-10-11 10:39:53 -0500

    clustering the weighted graph using MCL

---
+++ cell_type_prediction/MCLconsensus.R
@@ -0,0 +1,72 @@
+setwd("C:/Users/Juan.XIe/Desktop/test1/")
+library(igraph)
+library(mclust)
+library(MCL)
+library(clues)
+
+RAW <-read.table("Yan_RPKM",header=T,sep="\t")   # ground truth cell type
+CellNum <-dim(RAW)[2]-1  # the number of cells 
+
+
+Graph <-read.csv('Yan_RPKM_graph.csv',header=T,sep=",")
+G <-graph.data.frame(Graph,directed = FALSE)  # convert file into graph
+A <- as_adjacency_matrix(G,type="both",attr="weight",names=TRUE)  # convert graph into adjacency matrix
+MCL_name <-rownames(A)   # the vertix
+Covered <-length(MCL_name)  # the #of covered cells
+	
+CLUST <-list()
+for (i in 1:100){
+	CLUST[[i]] <-mcl(A,addLoops = FALSE,inflation =i,max.iter=200)
+}
+
+KK <- as.data.frame(do.call(rbind,lapply(CLUST,'[[',1)))  # extract the number of clusters
+CAN_I <-c(which(as.numeric(as.character(KK$V1))>=2)) 	# results that has more than 5 clusters
+tt <-as.numeric(as.character(KK$V1))
+tt <-sort(table(tt),decreasing=T)[1]
+Final_K <-as.numeric(names(tt))
+		
+if (length(CAN_I)!=0){
+	MATRIX <-rep(0,Covered)%o%rep(0,Covered)
+	for (k in 1:length(CAN_I)){	
+		MCL_label <-CLUST[[CAN_I[k]]]$Cluster  # record the label
+		ClusterNum <-unique(MCL_label)   # record the number of clusters
+		TEMP <-rep(0,Covered)%o%rep(0,Covered)
+		temp <-rep(0,Covered) %o% rep(0,length(ClusterNum))
+		for (n in 1:length(ClusterNum)){
+			index <-which(MCL_label==ClusterNum[n])
+			temp[index,n] <-1
+			TEMP <-TEMP+temp[,n]%o%temp[,n] 
+		}
+		MATRIX <-MATRIX+TEMP
+	}
+	MATRIX <-MATRIX/length(CAN_I)
+	rownames(MATRIX) <-colnames(MATRIX) <-rownames(A)
+	hc <-hclust(dist(MATRIX))
+	memb <-cutree(hc,k=Final_K)
+	if (length(rownames(A)) ==CellNum){
+		label <-memb
+	}else{
+		LEFT <-setdiff(names(RAW)[-1],MCL_name)
+		LEFT_Cluster <-rep(Final_K+1,length(LEFT))
+		df_cell_label <-data.frame(cell=c(names(memb),LEFT),cluster=c(memb,LEFT_Cluster),K=rep(Final_K+1,CellNum))				
+		label <-df_cell_label$cluster
+	}	
+}
+
+head(label)
+
+target <-read.table('Yan_cell.csv',header=T,sep=',')
+head(target)
+sorted <-label[match(target$Cell_type,names(label))]
+
+ARI <-adjustedRandIndex(sorted,target$Cluster)  # calculate ARI
+RI <-adjustedRand(sorted,target$Cluster,randMethod='Rand')
+FM <-adjustedRand(sorted,target$Cluster,randMethod='FM')
+JI <-adjustedRand(sorted,target$Cluster,randMethod='Jaccard')
+
+
+
+
+
+
+
